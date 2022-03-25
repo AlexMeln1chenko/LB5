ba30a93373ba9f475594fb27862e0644f6eedd20
AlexMeln1chenko <aleks.melnichenko@inbox.ru>
2022-03-26 01:12:07 +0200

    LB5

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(212, 383);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Информация";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(230, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(558, 426);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Созданные элементы";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(125, 401);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Создать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Вариант 9";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                foreach (Control k in groupBox1.Controls)
+                    groupBox1.Controls.Remove(k);
+            }
+            Random r1 = new Random();
+            for (int j = 0; j <= 2; j++)
+            {
+                for (int i = 0; i <= 3; i++)
+                {
+                    int x = r1.Next(20, 50);
+                    int k = r1.Next(1, 6);
+                    switch (k)
+                    {
+                        case 1:
+                            Button a = new Button();
+                            a.Parent = groupBox1;
+                            a.Location = new Point(40 + 200 * j, 40 + 100 * i);
+                            a.Size = new Size(x, 20); break;
+                        case 2:
+                            ListBox b = new ListBox();
+                            b.Parent = groupBox1;
+                            b.Location = new Point(40 + 200 * j, 40 + 100 * i);
+                            b.Size = new Size(x, 20);
+                            b.BackColor = Color.BlueViolet; break;
+                        case 3:
+                            TextBox c = new TextBox();
+                            c.Parent = groupBox1;
+                            c.Location = new Point(40 + 200 * j, 40 + 100 * i);
+                            c.Size = new Size(x, 20); break;
+                        case 4:
+                            GroupBox d = new GroupBox();
+                            d.Parent = groupBox1;
+                            d.Location = new Point(40 + 200 * j, 40 + 100 * i);
+                            d.Size = new Size(x, 20);
+                            d.BackColor = Color.RoyalBlue; break;
+                        case 5:
+                            DataGridView f = new DataGridView();
+                            f.Parent = groupBox1;
+                            f.Location = new Point(40 + 200 * j, 40 + 100 * i);
+                            f.Size = new Size(x, 20); break;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 0, b = 0, c = 0, d = 0, f = 0;
+            foreach (Control k in groupBox1.Controls)
+            {
+                if (k is Button)
+                    a++;
+                if (k is ListBox)
+                    b++;
+                if (k is TextBox)
+                    c++;
+                if (k is GroupBox)
+                    d++;
+                if (k is DataGridView)
+                    f++;
+            }
+            textBox1.Text = " Button: " + a.ToString() + Environment.NewLine + " ListBox: " + b.ToString() + Environment.NewLine + " TextBox: " + c.ToString()
+                + Environment.NewLine + " GroupBox: " + d.ToString() + Environment.NewLine + " DataGridView: " + f.ToString();
+        }
+    }
+}
